eb36adad81d2f3ee0634a2072ff81e3c5aafc3b6
lorranirodriguess <146475221+lorranirodriguess@users.noreply.github.com>
2025-04-06 17:29:14 -0300

    ok

--- portfolio3/navegacao.js
+++ portfolio3/navegacao.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 let botaoNavegacao = document.getElementById('botao-abrir-navegacao')
 let navegacao = document.getElementById('navegacao-mobile')
 
@@ -10,18 +8,3 @@ botaoNavegacao.addEventListener('click', ()=>{
 navegacao.addEventListener('click', ()=>{
     navegacao.classList.remove('abrir-menu')
 })
-
-=======
-
-let botaoNavegacao = document.getElementById('botao-abrir-navegacao')
-let navegacao = document.getElementById('navegacao-mobile')
-
-botaoNavegacao.addEventListener('click', ()=>{
-    navegacao.classList.add('abrir-menu')
-})
-
-navegacao.addEventListener('click', ()=>{
-    navegacao.classList.remove('abrir-menu')
-})
-
->>>>>>> b7b9fd20bd170dcd62f8a067373332f2d589a8c3
