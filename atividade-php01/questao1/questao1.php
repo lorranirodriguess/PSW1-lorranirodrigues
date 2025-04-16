f980d47ac1daa305c38f1bb6d772b5586a9301ed
lorranirodriguess <146475221+lorranirodriguess@users.noreply.github.com>
2025-04-16 14:52:55 -0300

    ok

---
+++ atividade-php01/questao1/questao1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questão 01</title>
+</head>
+<style></style>
+<body>
+
+    <main>
+        <h1>Sua média</h1>
+        <p> 
+            <?php 
+                $nota1 = $_GET["nota1"];
+                $nota2 = $_GET["nota2"];
+                $nota3 = $_GET["nota3"];
+                $nota4 = $_GET["nota4"];
+
+                $media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+
+                echo "A média das suas notas é $media";
+            ?>
+        </p>
+    </main>    
+
+</body>
+</html>
