f6a5eca7b261cfc2fee98920ea6ad988a28548f1
lorranirodriguess <146475221+lorranirodriguess@users.noreply.github.com>
2025-04-16 14:53:11 -0300

    ok

--- atividade-php01/questao1/questao1.php
+++ atividade-php01/questao1/questao1.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Questão 01</title>
 </head>
 <style></style>
@@ -12,10 +13,10 @@
         <h1>Sua média</h1>
         <p> 
             <?php 
-                $nota1 = $_GET["nota1"];
-                $nota2 = $_GET["nota2"];
-                $nota3 = $_GET["nota3"];
-                $nota4 = $_GET["nota4"];
+                $nota1 = $_REQUEST["nota1"];
+                $nota2 = $_REQUEST["nota2"];
+                $nota3 = $_REQUEST["nota3"];
+                $nota4 = $_REQUEST["nota4"];
 
                 $media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
 
